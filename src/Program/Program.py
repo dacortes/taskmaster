81d5fc78bee1db5ebceb187bd31b350be137ed9e
Luis <luissoto2094@gmail.com>
2025-08-22 21:24:22 +0200

    [FIX] no usa ProgramnProcess

--- src/Program/Program.py
+++ src/Program/Program.py
@@ -1,11 +1,12 @@
 from Program.ProgramConfig.ProgramConfig import ProgramConfig
-from Program.ProgramProcess.ProgramProcess import ProgramProcess
+
+# from Program.ProgramProcess.ProgramProcess import ProgramProcess
 
 
 class Program:
     def __init__(self, dict: dict = None):
         self.program_config = ProgramConfig(dict)
-        self.process = ProgramProcess(self.program_config)
+        # self.process = ProgramProcess(self.program_config)
         # self.process = ProgramProcess(dict)
 
     def __repr__(self):
