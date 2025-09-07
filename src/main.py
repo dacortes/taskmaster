4b2a64817af77ccdf797abcd3d2d071d50bbaa21
Luis <luissoto2094@gmail.com>
2025-09-07 09:02:04 +0200

    chore: remove unused example

--- src/main.py
+++ src/main.py
@@ -5,48 +5,7 @@ import yaml
 
 from Constants import CONFIG_PATH
 from Logger import LOGGER as logger
-from Program.Program import Program
-from Program.ProgramConfig.ProgramConfig import ProgramConfig
 from TaskMaster import TaskMaster
-
-
-def example():
-    import os
-    import tempfile
-
-    import yaml
-
-    config_data1 = {
-        "name": "ls",
-        "command": "/bin/ls",
-        "processes": 1,
-        "env": {"PORT": "8080", "foo": "loco"},
-        "expected_exit_codes": [0, 1],
-    }
-
-    config_data2 = {
-        "name": "loco",
-        "command": "/bin/ls -la",
-        "processes": 1,
-        "expected_exit_codes": [0, 1],
-    }
-    with tempfile.NamedTemporaryFile(
-        delete=False, suffix=".yaml", mode="w"
-    ) as tmp_file:
-        yaml.dump(config_data1, tmp_file)
-        tmp_file_path = tmp_file.name
-    config = ProgramConfig(tmp_file_path)
-    if config:
-        print("Ok")
-    os.unlink(tmp_file_path)
-    tmp = {}
-    tmp.update({config_data1.get("name"): config_data1})
-    tmp.update({config_data2.get("name"): config_data2})
-    try:
-        pro = Program(tmp)
-        print(pro)
-    except Exception as err:
-        print(err)
 
 
 def get_args():
@@ -86,7 +45,6 @@ def get_tm(args):
 
 
 if __name__ == "__main__":
-    # example()
     args = get_args()
     tm = get_tm(args)
     tm.startProcess("ls")
